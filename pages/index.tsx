d10bd8a0807dde2e25def80b7a0a29db4d105634
gonormonor <gonormonor@gmail.com>
2022-11-08 15:54:25 +0000

    blogistics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import Header from "../components/partials/Header";
-import HeroHome from "../components/partials/HeroHome";
-import FeaturesHome from "../components/partials/Features";
-import FeaturesBlocks from "../components/partials/FeaturesBlocks";
-import Testimonials from "../components/partials/Testimonials";
-import Newsletter from "../components/partials/Newsletter";
-import Footer from "../components/partials/Footer";
+
 
 function Index() {
   return (
